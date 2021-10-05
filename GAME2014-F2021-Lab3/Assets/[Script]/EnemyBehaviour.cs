78b7df22bc5a8cda1ec772a580df7a1c559f8589
Nathan Nguyen <zhectx@gmail.com>
2021-10-05 17:00:29 -0400

    Bullet Enemy Behaviour Scripts

--- GAME2014-F2021-Lab3/Assets/[Script]/EnemyBehaviour.cs
+++ GAME2014-F2021-Lab3/Assets/[Script]/EnemyBehaviour.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Bullet Movement")]
+    [Range(0.0f, 0.5f)]
+    public float speed;
+    public Bounds bulletBounds;
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position -= new Vector3(0.0f, speed, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y < bulletBounds.max)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
